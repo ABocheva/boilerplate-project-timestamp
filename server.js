eb2e7aebd5521f1eb4194c8ae2c5c0aaa4ccd624
Antoniya Bocheva <abocheva65@googlemail.com>
2019-05-16 08:47:05 +0100

    last changed to server.js

--- server.js
+++ server.js
@@ -6,17 +6,24 @@
 var express = require('express');
 var app = express();
 
-//declaring an array of months
-var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+app.use(function middleware(req, res, next) {
+console.log(req.method + ' ' + req.path + ' - ' + req.ip)
+next();
+});
+
 
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
-
+var getUnixTimestamp = date => ({
+  unix: date.getTime(),
+  utc: date.toUTCString()
+});
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-//create an instance of epress for our app and use bodyparser and cors
 app.use(cors({optionSuccessStatus: 200}));// some legacy browsers choke on 204
+
 app.use(bodyParser.json());
 
 // http://expressjs.com/en/starter/static-files.html
@@ -28,13 +35,30 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-
 //your first API endpoint... 
-app.get("/api/timestamp", function (req, res) {
- res.json({greeting: 'hello API'});
+//if the date string is empty it should trigger the current date
+app.get("/api/timestamp/", function (req, res) {
+  res.json(getUnixTimestamp(new Date()));
 });
 
+
+app.get("/api/timestamp/:date_string", function (req, res) {
+  let dateString = req.params.date_string;
+  var dateStringRegex = new RegExp(/\d{4}-\d{2}-\d{2}/g);
+  
+  let myDate; 
+  let isDate = dateStringRegex.test(dateString);
+  
+  if (isDate){
+    // we know its a date, e.g "2016-11-20"
+    myDate = new Date(dateString);
+  }else{
+    // we know it IS a number, so must be a timestamp
+    let parsedInt = parseInt(dateString);
+    myDate = new Date(parsedInt);
+  }
+ res.json(getUnixTimestamp(myDate));
+});
 
 
 // listen for requests :)
